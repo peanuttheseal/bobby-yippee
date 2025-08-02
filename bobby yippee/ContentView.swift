2e810556f9a813787438069f79556ae2620e33f9
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-08-02 14:25:08 +0800

    Initial Commit

---
+++ bobby yippee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bobby yippee
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bobby yippee/bobby_yippeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobby_yippeeApp.swift
+//  bobby yippee
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobby_yippeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
